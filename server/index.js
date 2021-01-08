204b30dc7e888d5eb4addc168d689b896af62c04
zsoe <zwemsoe@gmail.com>
2021-01-08 02:15:34 +0200

    clean up client

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const http = require("http").Server(app);
+const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 app.use(cors());
 
-app.listen(4000, () => {
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+http.listen(4000, () => {
   console.log("listening on 4000");
 });
