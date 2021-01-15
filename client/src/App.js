93520ca17668ab21e66e712dd2be046f056c5e67
zsoe <zwemsoe@gmail.com>
2021-01-14 21:14:32 -0800

    set up home page ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
-import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return <div className='App'>HELLO!</div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      <div className='centered whole-screen'>
+        <div>
+          <div className='card home-card'>
+            <div className='card-body'>
+              <input
+                className='form-control'
+                type='text'
+                placeholder='Your Name'
+              />
+              <div className='somespace'></div>
+              <div className='somespace'></div>
+              <button type='button' class='btn btn-secondary'>
+                Create a Room
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+export const Navbar = () => {
+  return (
+    <div>
+      <nav className='navbar navbar-light bg-light'>
+        <div className='container-fluid'>
+          <a className='navbar-brand' href='#'>
+            <img
+              src='/docs/5.0/assets/brand/bootstrap-logo.svg'
+              alt=''
+              width='30'
+              height='24'
+              className='d-inline-block align-top'
+            />
+            Bootstrap
+          </a>
+          <div className='d-flex'>
+            <a className='nav-link' href='#'>
+              <u>How To Play</u>
+            </a>
+            <a className='nav-link' href='#'>
+              <u>About</u>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
