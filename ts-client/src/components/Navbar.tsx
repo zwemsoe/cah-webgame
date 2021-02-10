fb3e5aee75a70773062f0670c42dd4b3c7de1c6e
zsoe <zwemsoe@gmail.com>
2021-02-10 13:41:03 -0800

    added tailwind

--- ts-client/src/components/Navbar.tsx
+++ ts-client/src/components/Navbar.tsx
@@ -1,30 +1,34 @@
 
 export const Navbar = () => {
     return (
-      <div>
-        <nav className='navbar navbar-light bg-light'>
-          <div className='container-fluid'>
-            <a className='navbar-brand' href='#'>
-              <img
-                src='/docs/5.0/assets/brand/bootstrap-logo.svg'
-                alt=''
-                width='30'
-                height='24'
-                className='d-inline-block align-top'
-              />
-              Bootstrap
-            </a>
-            <div className='d-flex'>
-              <a className='nav-link' href='#'>
-                <u>How To Play</u>
-              </a>
-              <a className='nav-link' href='#'>
-                <u>About</u>
-              </a>
-            </div>
-          </div>
-        </nav>
-      </div>
+      <header className="border-b md:flex md:items-center md:justify-between p-4 pb-0 shadow-lg md:pb-4 rounded-b-xl bg-black">
+        <div className="flex items-center justify-between mb-4 md:mb-0">
+    <h1 className="leading-none text-2xl text-grey-darkest">
+      <a className="no-underline text-white hover:text-black" href="/#">
+        Site Title
+      </a>
+    </h1>
+  </div>
+  <nav>
+    <ul className="list-reset md:flex md:items-center">
+      <li className="md:ml-4">
+        <a className="block no-underline hover:underline py-2 text-white hover:text-black md:border-none md:p-0" href="/#">
+          How to Play
+        </a>
+      </li>
+      <li className="md:ml-4">
+        <a className="border-t block no-underline hover:underline py-2 text-white hover:text-black md:border-none md:p-0" href="/#">
+          About
+        </a>
+      </li>
+      <li className="md:ml-4">
+        <a className="border-t block no-underline hover:underline py-2 text-white hover:text-black md:border-none md:p-0" href="/#">
+          Contact
+        </a>
+      </li>
+    </ul>
+  </nav>
+      </header>
     );
   };
   
