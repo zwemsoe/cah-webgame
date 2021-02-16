2128de10af6009b0503335120d60a4ef77ff6bdd
zsoe <zwemsoe@gmail.com>
2021-02-16 01:29:31 -0800

    re-routing fix

--- ts-client/src/screens/Home.tsx
+++ ts-client/src/screens/Home.tsx
@@ -1,25 +1,33 @@
 import { useState, useEffect, useContext, useRef } from "react";
 import { Navbar } from "../components/Navbar";
-import { randomString } from "../utils";
 import { SocketContext } from "../contexts/SocketContext";
+import { v4 as uuid } from 'uuid';
 
 interface Props {
-  history: any;
+  history: any,
+  setClientName: any,
+  match: any,
 }
 
-export default function Home({ history }: Props) {
+export default function Home({ history, setClientName, match }: Props) {
   const [name, setName] = useState("");
   const [code, setCode] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const [randomCode, setRandomCode] = useState("");
-  const codeInputRef = useRef<HTMLInputElement | null>(null);
   const socket = useContext(SocketContext);
-  useEffect(() => {});
+  
+  useEffect(() => {
+    const roomCode = match.params.roomId;
+    if(match.params.roomId){
+      setCode(roomCode);
+    } else {
+      setCode(uuid());
+    }
+  }, []);
 
   const handleJoin = () => {
     setLoading(true);
 
-    const clientId = randomString(16);
+    const clientId = uuid();
     socket.emit("join room", {
       roomCode: code,
       clientName: name,
@@ -28,16 +36,10 @@ export default function Home({ history }: Props) {
 
     setTimeout(() => {
       history.push("/" + code, { clientId });
+      setClientName(name);
     }, 2000);
   };
 
-  const handleCreateRoomCode = () => {
-    const new_code = randomString(10);
-    setRandomCode(new_code);
-    setCode(new_code);
-    //@ts-ignore
-    codeInputRef.current.value = new_code;
-  };
 
   if (isLoading) {
     return (
@@ -61,28 +63,12 @@ export default function Home({ history }: Props) {
                 setName(e.target.value);
               }}
             />
-            <input
-              className="rounded-sm px-4 py-3 mt-3 focus:outline-none bg-gray-100 w-full mb-2"
-              type="text"
-              name="room-name"
-              placeholder="Room Code"
-              ref={codeInputRef}
-              onChange={(e) => {
-                setCode(e.target.value);
-              }}
-            />
             <div className="grid grid-flow-col grid-cols-2 mt-3 gap-2">
               <button
                 className="bg-yellow-500 hover:bg-yellow-300 text-black font-bold h-10 rounded-full"
                 onClick={handleJoin}
               >
                 Join Room
-              </button>
-              <button
-                className="bg-black hover:bg-gray-700 text-white font-bold h-10 rounded-full"
-                onClick={handleCreateRoomCode}
-              >
-                Create a Room Code
               </button>
             </div>
           </div>
