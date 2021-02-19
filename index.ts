ed6671c2eda1796b728cb9e17822b125f999b57f
zsoe <zwemsoe@gmail.com>
2021-02-18 18:35:55 -0800

    changed server

--- index.ts
+++ index.ts
@@ -8,18 +8,9 @@ const server = require("http").createServer(app);
 app.use(cors());
 sockets(server);
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static('ts-client/build'));
-
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'ts-client', 'build', 'index.html'));
-  });
-}
+app.get('/', (req, res) => {
+  res.send("Serving for app deployed at https://cah-webgame.firebaseapp.com/")
+});
 
 
 const port = process.env.PORT || 5000;
