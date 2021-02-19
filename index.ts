1c7b167df2de8006d24a247289c433810d9f83e6
zsoe <zwemsoe@gmail.com>
2021-02-18 16:31:33 -0800

    deployment config

--- index.ts
+++ index.ts
@@ -12,6 +12,19 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Index');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('ts-client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'ts-client', 'build', 'index.html'));
+  });
+}
+
 
 const port = process.env.PORT || 5000;
 
