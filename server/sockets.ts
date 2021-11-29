7c10c82c07450707cbabdfb8f2e547dee287a8ec
zwe <zwemsoe@gmail.com>
2021-11-29 12:30:33 -0800

    remove video

--- server/sockets.ts
+++ server/sockets.ts
@@ -1,21 +1,20 @@
-import { config } from './constants';
+import { config } from "./constants";
 
 const url = config.url.CLIENT_URL;
 
 export default (server: any) => {
-    const io = require('socket.io')(server, {
-        cors: {
-            origin: url,
-            credentials: true,
-            pingTimeout: 30000,
-        },
-    });
+  const io = require("socket.io")(server, {
+    cors: {
+      origin: url,
+      credentials: true,
+      pingTimeout: 30000,
+    },
+  });
 
-    io.on("connection", (socket: any) => {
-        require('./handlers/user-handler')(socket, io);
-        require('./handlers/game-handler')(socket, io);
-        require('./handlers/video-handler')(socket, io);
-    });
+  io.on("connection", (socket: any) => {
+    require("./handlers/user-handler")(socket, io);
+    require("./handlers/game-handler")(socket, io);
+  });
 
-    return io;
-}
+  return io;
+};
--- ts-client/src/screens/Room.tsx
+++ ts-client/src/screens/Room.tsx
@@ -4,8 +4,6 @@ import { SocketContext } from "../contexts/SocketContext";
 import GameRoom from "../components/game/GameRoom";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { User, Setting } from "../interfaces";
-import VideoPanel from "../components/video/VideoPanel";
-import Peer from "simple-peer";
 
 interface Props {
   history: any;
@@ -15,11 +13,6 @@ interface Props {
 var defaultSetting: Setting = {
   rounds: 3,
   toggleNSFW: false,
-};
-
-const videoConstraints = {
-  height: window.innerHeight / 2,
-  width: window.innerWidth / 2,
 };
 
 export default function Room({ match, history }: Props) {
@@ -33,21 +26,7 @@ export default function Room({ match, history }: Props) {
   const settingRef = useRef(defaultSetting);
   settingRef.current = setting;
 
-  const [peers, setPeers] = useState<any>([]);
-  const userVideo = useRef<any>();
-  const peersRef = useRef<any>([]);
-
   const socket = useContext(SocketContext);
-
-  useEffect(() => () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: videoConstraints, audio: true })
-      .then((mediaStream) => {
-        const stream = mediaStream;
-        const tracks = stream.getTracks();
-        if (tracks.length > 0) tracks[0].stop();
-      });
-  }, []);
 
   useEffect(() => {
     const { clientId, clientName } = history.location.state;
@@ -56,75 +35,6 @@ export default function Room({ match, history }: Props) {
       clientName: clientName,
       clientId: clientId,
     });
-  }, []);
-
-  useEffect(() => {
-    const { clientId } = history.location.state;
-    navigator.mediaDevices
-      .getUserMedia({ video: videoConstraints, audio: true })
-      .then((stream) => {
-        userVideo.current && (userVideo.current.srcObject = stream);
-
-        socket.emit("join video chat", roomCode);
-
-        socket.on("update peers", (users) => {
-          let new_peers = [];
-          users.forEach((user: any) => {
-            if (user.id !== clientId) {
-              const peer = createPeer(user.id, clientId, stream);
-              const peerExists = peersRef.current.findIndex(
-                (p) => p.peerId === user.id
-              );
-              if (peerExists > -1) {
-                peersRef.current.splice(peerExists, 1);
-              }
-              peersRef.current.push({
-                peerId: user.id,
-                peer,
-              });
-              //@ts-ignore
-              new_peers.push({ peerId: user.id, peer });
-            }
-          });
-          setPeers(new_peers);
-        });
-
-        socket.on("peer joined", (payload) => {
-          const peer = addPeer(payload.signal, payload.callerId, stream);
-          const peerExists = peersRef.current.findIndex(
-            (p) => p.peerId === payload.callerId
-          );
-          if (peerExists > -1) {
-            peersRef.current.splice(peerExists, 1);
-          }
-          peersRef.current.push({
-            peerId: payload.callerId,
-            peer,
-          });
-          setPeers((users) => [
-            ...users,
-            {
-              peerId: payload.callerId,
-              peer,
-            },
-          ]);
-        });
-
-        socket.on("receiving returned signal", (payload) => {
-          const item = peersRef.current.find((p) => p.peerId === payload.id);
-          item.peer.signal(payload.signal);
-        });
-
-        socket.on("user left video", (userId) => {
-          const item = peersRef.current.find((p) => p.peerId === userId);
-          if (item) {
-            item.peer.destroy();
-          }
-          const new_peers = peersRef.current.filter((p) => p.peerId !== userId);
-          peersRef.current = new_peers;
-          setPeers(new_peers);
-        });
-      });
   }, []);
 
   useEffect(() => {
@@ -158,43 +68,6 @@ export default function Room({ match, history }: Props) {
       setGameStarted(true);
     });
   });
-
-  const createPeer = (userToSignal: string, callerId: string, stream: any) => {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream,
-    });
-
-    peer.on("signal", (signal: any) => {
-      socket.emit("sending signal", {
-        userToSignal,
-        callerId,
-        signal,
-        roomCode,
-      });
-    });
-
-    return peer;
-  };
-
-  const addPeer = (incomingSignal: any, callerId: string, stream: any) => {
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream,
-    });
-
-    const { clientId } = history.location.state;
-
-    peer.on("signal", (signal: any) => {
-      socket.emit("returning signal", { signal, callerId, roomCode, clientId });
-    });
-
-    peer.signal(incomingSignal);
-
-    return peer;
-  };
 
   const handleSetting = (e: any) => {
     const { name, value, checked } = e.target;
@@ -235,7 +108,7 @@ export default function Room({ match, history }: Props) {
 
   return (
     <>
-      <div className='grid grid-cols-2'>
+      <div className="grid grid-cols-2">
         <div>
           {gameStarted ? (
             <GameRoom
@@ -259,9 +132,6 @@ export default function Room({ match, history }: Props) {
               />
             </>
           )}
-        </div>
-        <div style={{ position: "absolute", right: 0 }}>
-          <VideoPanel userVideo={userVideo} peers={peers} />
         </div>
       </div>
     </>
